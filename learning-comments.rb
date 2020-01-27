388442cb6015b0d2d097ca92e6128be4b3a8f20d
Franco Canedo <francanedo1@hotmail.com>
2020-01-27 02:42:38 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,8 @@
+# From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+year = Time.now.year
+puts year
